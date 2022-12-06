0700f970b4ef0be920d261956e05afb11f1070b5
idbaliyah <idbaliyah@gmail.com>
2022-12-06 16:03:09 +0700

    Update Login Scenario

---
+++ src/test/java/StepDefinition/RunTest.java
@@ -0,0 +1,18 @@
+package StepDefinition;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        tags = {"@Login, @Purchase"},
+        features = {"classpath:features/"},
+        glue = { "classpath:StepDefinition"},
+        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+)
+public class RunTest {
+
+}
+
